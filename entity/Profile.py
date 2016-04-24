d48f2fea35ce0b2d4b0fb5c4930153c393e12ef8
yamada.isao@gmail.com <natsuko3>
2016-04-24 16:53:13 +0900

    + ２時間でコンテキストを忘れるようにする + キャラ変更コマンドを追加

--- entity/Profile.py
+++ entity/Profile.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from google.appengine.ext import ndb
 
 class Profile(ndb.Model) :
   mid = ndb.StringProperty(indexed=True)
   context = ndb.StringProperty(indexed=False)
+  char = ndb.StringProperty(indexed=False)
   date = ndb.DateTimeProperty(auto_now_add=True)
--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 import logging
 import urllib
 import json
+import random
+from datetime import datetime, date, time
+
 from google.appengine.ext import vendor
 vendor.add('lib')
 from google.appengine.api import urlfetch
@@ -20,20 +23,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-#  prof = Profile()
-#  prof.mid = 'aaa'
-#  prof.context = 'context'
-#  prof.put()
   return 'hello my line bot'
 
 @app.route('/callback', methods=["POST"])
 def linebot():
+  """ LINEのコールパック """
   args = json.loads(request.get_data().decode('utf-8'))
 #  logging.debug('kick from line server,\n %s'%(args['result']))
   queue = taskqueue.Queue('bot-task')
 
   for msg in args['result']:
-#    logging.debug(json.dumps(msg))
     task = taskqueue.Task(url='/_ah/queue/bot-reply', params={'param': json.dumps(msg) }, method="POST")
     queue.add(task)
   return "{}"
@@ -41,20 +40,55 @@ def linebot():
 @app.route('/_ah/queue/bot-reply', methods=["POST"])
 def reply():
   msg = json.loads(request.form.get('param').decode('utf-8'))
+  logging.debug(json.dumps(msg))
   prof = Profile.get_or_insert(msg["content"]["from"], mid=msg["content"]["from"])
-  res = dialogue(msg["content"]["text"], prof.context, 'dialog' )
+
+  # 時間がたったらコンテキストを忘れる
+  diff = datetime.now() - prof.date
+  if diff.microseconds > 72000000 :
+      prof.context = ""
+      # キャラも適当に変える
+      prof.char = changeChar(random.randint(1, 3))
+      logging.debug("context is clear")
+  # キャラ変更コマンド
+  prof.char = charCommand(msg, prof.char)
+
+  res = dialogue(msg["content"]["text"], prof.context, 'dialog', prof.char )
   prof.context = res["context"]
   prof.put()
-  kickBot( msg["content"]["from"], msg["eventType"], res["utt"] )
+  sendLine(msg["content"]["from"], msg["eventType"], res["utt"])
   return "{}"
 
-def dialogue(msg, context, mode):
+def charCommand(msg, defchar) :
+  char = ""
+  # キャラのコマンド
+  if msg["content"]["text"].find(u"普通") >= 0:
+      char = ""
+  if msg["content"]["text"].find(u"関西") >= 0:
+      char = "20"
+  if msg["content"]["text"].find(u"赤ちゃん") >= 0:
+      char = "30"
+  if defchar is None:
+      char = changeChar(random.randint(1, 3))
+  return char
+
+def changeChar(n):
+    """ キャラ変更の辞書 """
+    switch = {
+        1: "",
+        2: "20",
+        3: "30"
+    }
+    return switch.get(n) or ''
+
+def dialogue(msg, context, mode, t):
   url = config.DOCOMO_DIALOGUE_URL + config.DOCOMO_DIALOGUE_API_KEY
   form_fields = {
       "utt": msg,
       "context": context,
-      "mode": mode
-      }
+      "mode": mode,
+      "t": t
+  }
   result = urlfetch.fetch(
     url=url,
     payload=json.dumps(form_fields,ensure_ascii=False),
@@ -66,10 +100,10 @@ def dialogue(msg, context, mode):
   if result.status_code == 200:
     logging.debug(result.content)
   else:
-    logging.debug(result.content)
+    logging.warn(result.content)
   return json.loads(result.content)
 
-def kickBot(tgt_id, event_type, msg_data):
+def sendLine(tgt_id, event_type, msg_data):
   url = config.LINE_URL
   form_fields = {
       "to": [str(tgt_id)],
@@ -97,4 +131,4 @@ def kickBot(tgt_id, event_type, msg_data):
   if result.status_code == 200:
     logging.debug(result.content)
   else:
-    logging.debug(result.content)
+    logging.warn(result.content)
